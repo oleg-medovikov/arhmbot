635b035e4e32f2c0d1212dc49577abfd27c4058e
oleg-medovikov <oleg.medovikov@mail.ru>
2023-02-06 10:10:29 +0300

    add dialog table

---
+++ base/dialogs.py
@@ -0,0 +1,20 @@
+from base import metadata
+
+from sqlalchemy import Table, Column, ARRAY, \
+    SmallInteger, String, DateTime
+
+t_dialogs = Table(
+        metadata,
+        'dialogs',
+        Column('d_id',        SmallInteger),  # идентификатор диалога
+        Column('q_id',        SmallInteger),  # идентификатор вопроса
+        Column('name',        String),  # название диалога
+        Column('description', String),  # описание ситуации
+        Column('answers',     ARRAY(String)),  # список ответов
+        Column('transfer',    ARRAY(SmallInteger)),  # куда переходим
+        Column('buy_items',   ARRAY(SmallInteger)),  # список товаров
+        Column('buy_costs',   ARRAY(SmallInteger)),  # список цен
+        Column('sele_items',  ARRAY(SmallInteger)),  # список на продажу
+        Column('sale_costs',  ARRAY(SmallInteger)),  # цены продажи
+        Column('date_update', DateTime),
+        )
--- base/inventory.py
+++ base/inventory.py
@@ -1,7 +1,7 @@
 from base import metadata
 
-from sqlalchemy import Table, Column, Integer, SmallInteger, String, DateTime
-
+from sqlalchemy import Table, Column, ARRAY, \
+    Integer, SmallInteger, String, DateTime
 
 t_inventory = Table(
         'inventory',
@@ -11,3 +11,21 @@ t_inventory = Table(
         Column('i_id', SmallInteger),
         Column('date_update', DateTime),
         )
+"""
+t_inventory = Table(
+        'inventory',
+        metadata,
+        Column('p_id',         Integer),  # идентификатор персонажа
+        Column('head',         SmallInteger),  # голова
+        Columns('earrings',    SmallInteger),  # серьги
+        Column('hand_one',     SmallInteger),  # правая рука
+        Column('hand_two',     SmallInteger),  # левая рука
+        Column('rings',        ARRAY(SmallInteger)),  # кольца
+        Column('body',         SmallInteger),  # костюм
+        Column('legs',         SmallInteger),  # штаны
+        Column('shoes',        SmallInteger),  # обувь
+        Column('bag',          ARRAY(SmallInteger)),  # сумка
+        Column('achievements', ARRAY(SmallInteger)),  # достижения
+        Column('date_update',  DateTime),
+        )
+"""

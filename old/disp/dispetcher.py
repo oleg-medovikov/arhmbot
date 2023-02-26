e3303732047c30b252b72c7dd0f3c5fbcc5049f3
oleg-medovikov <oleg.medovikov@mail.ru>
2023-02-26 21:45:50 +0300

    remove old

--- old/disp/dispetcher.py
+++
@@ -1,6 +0,0 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from conf import BOT_API
-
-bot = Bot(token=BOT_API)
-dp  = Dispatcher(bot, storage=MemoryStorage())
--- old/disp/on_startup.py
+++
@@ -1,7 +0,0 @@
-from func import set_default_commands
-from .dispetcher import dp
-import asyncio 
-
-async def on_startup(dp):
-    await set_default_commands(dp)
-    await dp.start_polling()
